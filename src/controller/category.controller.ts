a22ad29662fa01ab7a9694ebe52dcf5f7a8cb8ff
Krisha Rajbhar <krishna.rajbhar@audviklabs.com>
2025-12-24 00:33:51 +0530

    category cache fix

--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { prisma } from "../prisma";
 import { CreateCategoryDTO } from "../dtos/category.dtos";
 import { ApiError } from "../utils/api_error.utils.";
+import { cache, CATEGORY_CACHE_PREFIX } from "../utils/cache";
 
 const createCategory = asyncHandler(async (req: Request<{}, {}, CreateCategoryDTO>, res: Response) => {
     const { name, description } = req.body;
@@ -11,6 +12,8 @@ const createCategory = asyncHandler(async (req: Request<{}, {}, CreateCategoryDT
         throw new ApiError(400, 'Category already exists');
     }
     await prisma.category.create({ data: { name: name.toLowerCase().trim(), description } });
+    // clear cache
+    cache.del(CATEGORY_CACHE_PREFIX);
     res.success("Category created successfully", 201);
 });
 
@@ -22,6 +25,8 @@ const updateCategory = asyncHandler(async (req: Request<{ id: string }, {}, Crea
         throw new ApiError(404, 'Category not found');
     }
     const updatedCategory = await prisma.category.update({ where: { id: Number(id) }, data: { name: name.toLowerCase().trim(), description } });
+    // clear cache
+    cache.del(CATEGORY_CACHE_PREFIX + id);
     res.success("Category updated successfully", updatedCategory, 200);
 })
 
@@ -32,20 +37,36 @@ const deleteCategory = asyncHandler(async (req: Request<{ id: string }>, res: Re
         throw new ApiError(404, 'Category not found');
     }
     const deletedCategory = await prisma.category.delete({ where: { id: Number(id) } });
+    // clear cache
+    cache.del(CATEGORY_CACHE_PREFIX + id);
+    cache.del(CATEGORY_CACHE_PREFIX);
     res.success("Category deleted successfully", deletedCategory, 200);
 })
 
 const getAllCategories = asyncHandler(async (req: Request, res: Response) => {
+    const cachedCategories = await cache.get(CATEGORY_CACHE_PREFIX);
+    if (cachedCategories) {
+        return res.success('Categories fetched successfully', cachedCategories, 200);
+    }
+    // find all categories
     const categories = await prisma.category.findMany();
+    // cache all categories
+    cache.set(CATEGORY_CACHE_PREFIX, categories);
     res.success('Categories fetched successfully', categories, 200);
 })
 
 const getCategoryById = asyncHandler(async (req: Request<{ id: string }>, res: Response) => {
     const id = req.params.id
+    const cachedCategory = await cache.get(CATEGORY_CACHE_PREFIX + id);
+    if (cachedCategory) {
+        return res.success('Category fetched successfully', cachedCategory, 200);
+    }
     const category = await prisma.category.findUnique({ where: { id: Number(id) }, include: { courses: true } });
     if (!category) {
         throw new ApiError(404, 'Category not found');
     }
+    // cache category
+    cache.set(CATEGORY_CACHE_PREFIX + id, category);
     res.success('Category fetched successfully', category, 200);
 })
 export {
--- src/utils/send_mail.utils.ts
+++ src/utils/send_mail.utils.ts
@@ -1,23 +1,45 @@
 
-import { logger } from '../config/logger.config';
-import { transporter } from '../config/node_mailer.config';
-import { ApiError } from './api_error.utils.';
+// import { logger } from '../config/logger.config';
+// import { transporter } from '../config/node_mailer.config';
+// import { ApiError } from './api_error.utils.';
 
+// export const sendMail = async (to: string, subject: string, html: string) => {
+//   try {
+//     // not using await here to avoid blocking
+//     const info = await transporter.sendMail({
+//       from: `"QURAN LMS" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       html,
+//     });
+//     logger.info(`Email sent to ${to}`);
+//     return info;
+//   } catch (error: any) {
+//     logger.error(`Unable to send email: ${error.message || error}`);
+//     throw new ApiError(429, `Unable to send email: ${error.message || error}`);
+//   }
+// };
+import { Resend } from "resend";
+import { logger } from "../config/logger.config";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async (to: string, subject: string, html: string) => {
   try {
-    // not using await here to avoid blocking
-    const info = await transporter.sendMail({
-      from: `"QURAN LMS" <${process.env.EMAIL_USER}>`,
+    logger.info(`Message send to ${to}`)
+
+    const result = await resend.emails.send({
+      from: "onboarding@resend.dev",
       to,
       subject,
       html,
     });
-    logger.info(`Email sent to ${to}`);
-    return info;
-  } catch (error: any) {
-    logger.error(`Unable to send email: ${error.message || error}`);
-    throw new ApiError(429, `Unable to send email: ${error.message || error}`);
+
+
+    logger.info("✅ Resend result:", result)
+    return result;
+  } catch (err) {
+    logger.error("❌ Resend error:", err)
+    throw err;
   }
 };
